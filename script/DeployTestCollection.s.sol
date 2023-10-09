a43c0238ac33ecaaf690d1c02e8b65c36f5d8033
FredrikCarlssn <113025364+FredrikCarlssn@users.noreply.github.com>
2023-10-09 19:46:47 +0700

    localDeploy script added

---
+++ script/DeployTestCollection.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {WeaponTestCollection} from "src/WeaponTestCollection.sol";
+
+contract DeployTestCollection is Script {
+    function run() public returns (WeaponTestCollection) {
+        vm.startBroadcast();
+        WeaponTestCollection weaponTestCollection = new WeaponTestCollection();
+        vm.stopBroadcast();
+        return weaponTestCollection;
+    }
+}

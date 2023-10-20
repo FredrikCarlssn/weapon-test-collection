c563efd84d308d4c59577e50a33faf2525d1c70f
FredrikCarlssn <113025364+FredrikCarlssn@users.noreply.github.com>
2023-10-20 14:55:38 +0700

    small update to randomnumber

--- src/RandomNumber.sol
+++ src/RandomNumber.sol
@@ -5,13 +5,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract RandomNumberGeneratorNFT is ERC721Enumerable {
-    uint256 private seed;
-
-    constructor() ERC721("RandomNFT", "RNFT") {
-        seed = block.timestamp;
-    }
+    constructor() ERC721("RandomNFT", "RNFT") {}
 
     function getRandomNumber(uint8 _maxNumber) internal view returns (uint256) {
+        uint256 seed = block.timestamp;
         uint256 randomNumber = uint256(
             keccak256(
                 abi.encodePacked(
@@ -25,11 +22,9 @@ contract RandomNumberGeneratorNFT is ERC721Enumerable {
         return randomNumber;
     }
 
-    function mintNFTs(uint256 numNFTs) public {
-        require(numNFTs > 0, "Number of NFTs must be greater than 0");
+    function mintMultipleNFTs(uint256 numNFTs) public {
         for (uint256 i = 0; i < numNFTs; i++) {
-            uint256 tokenId = totalSupply() + 1; // Update the tokenId
-            _mint(msg.sender, tokenId);
+            mintNft(msg.sender, getRandomNumber(10));
         }
     }
 }

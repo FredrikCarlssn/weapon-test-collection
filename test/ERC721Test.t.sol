c316baaa1f7c0d0811d4a795aba24bdaef586b74
FredrikCarlssn <113025364+FredrikCarlssn@users.noreply.github.com>
2023-10-23 17:08:05 +0200

    security improvements and small cleanup

--- test/ERC721Test.t.sol
+++ test/ERC721Test.t.sol
@@ -14,16 +14,17 @@ contract NFTTest is Test {
     function setUp() public {
         vm.prank(msg.sender);
         weaponTestCollection = new WeaponTestCollection();
+        vm.prank(msg.sender);
         weaponTestCollection.mintNft(msg.sender, 0);
     }
 
     function testUri() public {
         string memory uri = weaponTestCollection.tokenURI(0);
-        console.log(uri);
         assertEq(uri, DEFAULT_URI);
     }
 
     function testMint() public {
+        vm.prank(msg.sender);
         weaponTestCollection.mintNft(msg.sender, 0);
         uint256 supply = weaponTestCollection.totalSupply();
         assertEq(supply, 2);
@@ -35,171 +36,12 @@ contract NFTTest is Test {
     }
 
     function testBatchMint() public {
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
-        weaponTestCollection.mintNft(msg.sender, 0);
+        for (uint256 i = 0; i < 99; i++) {
+            vm.prank(msg.sender);
+            weaponTestCollection.mintNft(msg.sender, 0);
+        }
         assertEq(weaponTestCollection.totalSupply(), 100);
     }
-
-    // function testFailMintNoPaid() public {
-    //     weaponTestCollection.mintNft(msg.sender, DEFAULT_URI, 0);
-    //     vm.expectRevert("wrongpayment()");
-    // }
-
-    // function testCorrectMintPrice() public {
-    //     weaponTestCollection.mintNft(msg.sender, DEFAULT_URI, 0);
-    // }
-
-    // function testMintAndSuppply() public {
-    //     weaponTestCollection.mintNft(msg.sender, DEFAULT_URI, 0);
-    //     weaponTestCollection.mintNft(msg.sender, DEFAULT_URI, 0);
-    //     uint256 supply = weaponTestCollection.totalSupply();
-    //     assertEq(supply, 12);
-    // }
-
-    // function testFailMintToZeroAddress() public {
-    //     address zero = address(0);
-    //     hoax(zero, 2 ether);
-    //     weaponTestCollection.mintNft{value: 1 ether}(zero, DEFAULT_URI, 0);
-    //     vm.expectRevert("ERC721: mint to the zero address");
-    // }
-
-    // function testFailUnSafeContractReceiver() public {
-    //     vm.etch(address(1), bytes("mock code"));
-    //     vm.prank(address(1));
-    //     weaponTestCollection.mintNft{value: 1 ether}(
-    //         msg.sender,
-    //         DEFAULT_URI,
-    //         0
-    //     );
-    // }
-
-    // function testreveal() public {
-    //     address bob = vm.addr(8);
-    //     hoax(bob, 2 ether);
-    //     weaponTestCollection.mintNft{value: 1 ether}(bob, DEFAULT_URI, 0);
-    //     string memory meta = DEFAULT_URI;
-    //     string memory uri = weaponTestCollection.uri(0);
-    //     console.log("URI1: ", weaponTestCollection.uri(0));
-    //     assertEq(uri, meta);
-    // }
-
-    // function testFailMetadataQuery() public {
-    //     address bob = vm.addr(6);
-    //     startHoax(bob, 2 ether);
-    //     weaponTestCollection.uri(1);
-    //     vm.expectRevert("nonexistenttoken()");
-    // }
-
-    // function bytes32ToString(
-    //     bytes32 _bytes32
-    // ) public pure returns (string memory) {
-    //     uint8 i = 0;
-    //     while (i < 32 && _bytes32[i] != 0) {
-    //         i++;
-    //     }
-    //     bytes memory bytesArray = new bytes(i);
-    //     for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
-    //         bytesArray[i] = _bytes32[i];
-    //     }
-    //     return string(bytesArray);
-    // }
 
     receive() external payable {}
 }
